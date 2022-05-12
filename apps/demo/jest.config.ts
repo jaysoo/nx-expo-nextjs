96be638b95847685b9c8e0018a356cce37df02f2
Jack Hsu <jack.hsu@gmail.com>
2022-05-12 13:11:40 -0400

    add demo app

---
+++ apps/demo/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'demo',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/demo',
+};
